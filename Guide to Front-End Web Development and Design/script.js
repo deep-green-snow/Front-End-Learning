5b49073f439b212bda0593130d7cb5fbac26dfd8
haaajh <dldudvy777@gmail.com>
2021-07-20 21:53:18 +0900

    add js

--- Guide to Front-End Web Development and Design/script.js
+++ Guide to Front-End Web Development and Design/script.js
@@ -1,1 +1,11 @@
 alert("from file");
+
+document.getElementById("btn2").addEventListener("click", function () {
+    alert('Hello 2');
+})
+document.getElementById("btn3").addEventListener("click", function () {
+    alert('Helllo\nWor\nld'); //Test out line breaks
+    /*
+    this is a comment
+    */
+})
