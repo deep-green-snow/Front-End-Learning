6411d41861fd22dd740b3758db9283e64611970f
haaajh <dldudvy777@gmail.com>
2021-09-01 23:06:51 +0900

    food commit

---
+++ The Complete React Native/rn-starter/src/components/ColorCounter.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const ColorCounter = (props) => {
+    return (
+    <View>
+        <Text>{props.color}</Text>
+        <Button 
+            onPress={() => props.onIncrease()}
+            title={`Increase ${props.color}`}
+        />
+        <Button 
+            onPress={() => props.onDecrease()}
+            title={`Decrease ${props.color}`}/>
+    </View>
+    );
+};
+
+
+const Styles = StyleSheet.create({});
+
+export default ColorCounter;
