c8486ee798cc848eebaaa5c58e3d44705d355f82
haaajh <dldudvy777@gmail.com>
2021-09-04 11:32:17 +0900

    delete token

--- The Complete React Native/food/src/api/yelp.js
+++ The Complete React Native/food/src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer 0w5EtkphTHG-WE0j6rEp_IVQYgA2yldUD64UaReK6SP723LX3spu8lFCHRYHyv-Z2jMhS13DcjMe_c7h_vU-oLoTvH-RuTfSVU9orU-fmSnFPl4U5iyUDBhRGIgvYXYx'
+        Authorization: 'Bearer '
     }
 });
