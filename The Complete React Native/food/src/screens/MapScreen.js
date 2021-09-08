02fe453873fce0df035258e401660adc8cbbd7ac
haaajh <dldudvy777@gmail.com>
2021-09-08 20:37:00 +0900

    map

--- The Complete React Native/food/src/screens/MapScreen.js
+++ The Complete React Native/food/src/screens/MapScreen.js
@@ -1,12 +1,63 @@
 import React, { useState, useEffect } from 'react';
-import {View, Text, StyleSheet, Dimensions } from 'react-native';
+import {View, Button, Text, StyleSheet, Dimensions } from 'react-native';
 import MapView from 'react-native-maps'
+import * as Location from 'expo-location'
 
 const MapScreen = () => {
+    const [location, setLocation] = useState(null);
+    const [currentLocation, setCurrentLocation] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            let {status} = await Location.requestForegroundPermissionsAsync();
+            if(status !== 'granted') {
+                console.log('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+            console.log(location);
+        })
+    }, []);
+
+    // console.log(currentLocation);
 
     return (
         <View style={styles.container}>
-            <MapView style={styles.map} />
+            <Button 
+                title="Current Location"
+                onPress={() => setCurrentLocation(location)}
+            />
+        <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        onRegionChange={region => {
+          setLocation({
+            latitude: region.latitude,
+            longitude: region.longitude,
+          });
+        }}
+        onRegionChangeComplete={region => {
+          setLocation({
+            latitude: region.latitude,
+            longitude: region.longitude,
+          });
+        }}>
+        <Marker
+          coordinate={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+          }}
+          title="this is a marker"
+          description="this is a marker example"
+        />
+      </MapView> 
         </View>
     );
 };
@@ -20,7 +71,7 @@ const styles = StyleSheet.create({
       },
     map: {
         width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
+        height: Dimensions.get('window').height - 100,
     },
 });
 
